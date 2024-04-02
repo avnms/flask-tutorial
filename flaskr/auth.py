c7ba64f66cf7f1e62ad9a0c0e13dcb8636c91900
avnms <sarmaakondivnm@icloud.com>
2024-04-03 10:16:23 +1100

    Validate and set user id data using before_app_request of the bp

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,9 +1,30 @@
-from flask import Blueprint, request, redirect, url_for, flash, render_template, session
+from flask import (
+    Blueprint,
+    request,
+    redirect,
+    url_for,
+    flash,
+    render_template,
+    session,
+    g,
+)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
 
 
 @bp.route("/register", methods=["GET", "POST"])
