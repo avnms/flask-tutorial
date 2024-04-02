afc3559c3a524cc7cbd6306107c9e35ec4b83bdd
avnms <sarmaakondivnm@icloud.com>
2024-04-03 10:26:48 +1100

    Format fix

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -25,7 +25,12 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = (
-            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+            get_db()
+            .execute(
+                "SELECT * FROM user WHERE id = ?",
+                (user_id,),
+            )
+            .fetchone()
         )
 
 
