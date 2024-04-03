c8ad61f521c79b7d9929f82db11c93d484e0e450
avnms <sarmaakondivnm@icloud.com>
2024-04-03 17:40:30 +1100

    Created function to get the post based on id and check_author flag

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,9 +1,29 @@
 from flask import Blueprint, render_template, request, flash, g, redirect, url_for
 
+from werkzeug.exceptions import abort
+
 from flaskr.db import get_db
 from flaskr.auth import login_required
 
 bp = Blueprint("blog", __name__)
+
+
+def get_post(id, check_author=True):
+    db = get_db()
+    post = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " WHERE p.id = ?",
+        (id,),
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post["author_id"] != g.user["id"]:
+        abort(403)
+
+    return post
 
 
 @bp.route("/")
