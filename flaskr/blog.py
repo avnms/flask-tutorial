7411d41073ba965d5f4ac7c0e546b5f66fc87aaf
avnms <sarmaakondivnm@icloud.com>
2024-04-03 17:46:10 +1100

    Created update route

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -38,7 +38,7 @@ def index():
     return render_template("blog/index.html", posts=posts)
 
 
-@bp.route("/create")
+@bp.route("/create", methods=["GET", "POST"])
 @login_required
 def create():
     if request.method == "POST":
@@ -61,3 +61,30 @@ def create():
             return redirect(url_for("blog.index"))
 
     return render_template("blog/create.html")
+
+
+@bp.route("/update", methods=["GET", "POST"])
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        error = None
+
+        if title is None:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "UPDATE post SET title = ?, body = ? WHERE id = ?",
+                (title, body, id),
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/update.html", post=post)
